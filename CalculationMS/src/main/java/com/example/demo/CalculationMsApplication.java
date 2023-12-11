7777a844fa3ddefc1836427c599a5f550ad419ba
myhaj <myhaj@yassine>
2023-12-12 00:28:04 +0100

    creating the second springboot application

---
+++ CalculationMS/src/main/java/com/example/demo/CalculationMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculationMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculationMsApplication.class, args);
+	}
+
+}
